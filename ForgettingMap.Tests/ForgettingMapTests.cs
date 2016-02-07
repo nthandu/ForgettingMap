b1f4c0b5ef246aa5c402f6f2083a82332aa208d8
nthandu <naresh.thandu@outlook.com>
2016-02-07 13:04:26 +0000

    initial changes

---
+++ ForgettingMap.Tests/ForgettingMapTests.cs
@@ -0,0 +1,91 @@
+﻿
+
+namespace ForgettingMap.Tests
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class ForgettingMapTests
+    {
+
+        // add association - key & value pair
+        [Test]
+        public void TestAddAssociation()
+        {
+            var forgettingMap = new ForgettingMap(2);
+            forgettingMap.Add("key", System.DateTime.Now.ToShortDateString());
+            Assert.AreEqual(1, forgettingMap.Count, "Expected count should be 1.");
+        }
+
+        // should not allow duplicate keys
+        [Test]
+        public void TestShouldNotAllowDuplicateKeys()
+        {
+            var forgettingMap = new ForgettingMap(2);
+            forgettingMap.Add("key", System.DateTime.Now.ToShortDateString());
+            Assert.Throws(typeof(System.ArgumentException), () => forgettingMap.Add("key", System.DateTime.Now.ToShortDateString()));
+        }
+
+        // should not exceed capacity
+        [Test]
+        public void TestShouldNotExceedCapacity()
+        {
+            int capacity = 2;
+            var forgettingMap = new ForgettingMap(capacity);
+            forgettingMap.Add("key", System.DateTime.Now.ToShortDateString());
+            forgettingMap.Add("key1", System.DateTime.Now.ToShortDateString());
+            forgettingMap.Add("key2", System.DateTime.Now.ToShortDateString());
+            Assert.AreEqual(capacity, forgettingMap.Count);
+        }
+        // find an association using key
+
+        // Keep track of find count
+        [Test]
+        public void TestKeepTrackOfSearchCount()
+        {
+            int capacity = 2;
+            var forgettingMap = new ForgettingMap(capacity);
+            forgettingMap.Add("key", System.DateTime.Now.ToShortDateString());
+            forgettingMap.Add("key2", System.DateTime.Now.ToShortDateString());
+            var searchCount = 5;
+            for (int i = 0; i < searchCount; i++)
+            {
+                var found = forgettingMap.Find("key");
+            }
+            Assert.AreEqual(searchCount, forgettingMap.SearchStats("key"));
+        }
+
+        // remove the least accessed association when capacity exceeds maximum
+        [Test]
+        public void TestShouldAddItemEvenIfCapacityExceedsByRemovingLeastAccessedItem()
+        {
+            int capacity = 2;
+            var forgettingMap = new ForgettingMap(capacity);
+            forgettingMap.Add("key", System.DateTime.Now.ToShortDateString());
+            forgettingMap.Add("key1", System.DateTime.Now.ToShortDateString());
+            forgettingMap.Add("key2", System.DateTime.Now.ToShortDateString());
+            Assert.IsNotNull(forgettingMap.Find("key2"));
+        }
+
+        // remove the least accessed association when capacity exceeds maximum
+        [Test]
+        public void TestShouldRemoveOldestItemIfTwoItemsWithLeastSearch()
+        {
+            int capacity = 2;
+            var forgettingMap = new ForgettingMap(capacity);
+            forgettingMap.Add("key", System.DateTime.Now.ToShortDateString());
+            forgettingMap.Find("key");
+            forgettingMap.Find("key");
+            forgettingMap.Find("key");
+
+            forgettingMap.Add("key1", System.DateTime.Now.ToShortDateString());
+            forgettingMap.Find("key1");
+            forgettingMap.Find("key1");
+            forgettingMap.Find("key1");
+            forgettingMap.Add("key2", System.DateTime.Now.ToShortDateString());
+            Assert.IsNotNull(forgettingMap.Find("key1"));
+        }
+    }
+
+    
+}
---
+++ ForgettingMap/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForgettingMap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public class ForgettingMap
+    {
+
+        private readonly IDictionary<string, string> _associations = null;
+        private readonly IDictionary<string, int> _searchStats = new Dictionary<string, int>();
+        private readonly IDictionary<string, DateTime> _timeOfInsertions = new Dictionary<string, DateTime>();
+        private readonly int _maxCapacity;
+        public ForgettingMap(int capacity)
+        {
+            _maxCapacity = capacity;
+            _associations = new Dictionary<string, string>(capacity);
+        }
+
+        public void Add(string key, string toShortDateString)
+        {
+            if (_associations.Count >= _maxCapacity)
+            {
+                if (_searchStats.Count > 0)
+                {
+                    // get least searcg count
+                    var leastSearchCount = _searchStats.Values.Min();
+                    var keysWithMinValues =
+                        _searchStats.Where(item => item.Value == leastSearchCount).Select(item => item.Key).ToList();
+
+                    var oldestItem =
+                        _timeOfInsertions.Where(item => keysWithMinValues.Contains(item.Key))
+                            .OrderBy(item => item.Value)
+                            .First()
+                            .Key;
+
+                    _associations.Remove(oldestItem);
+                }
+                else
+                {
+                    var oldestItem = _timeOfInsertions
+                            .OrderBy(item => item.Value)
+                            .First()
+                            .Key;
+                    _associations.Remove(oldestItem);
+                }
+            }
+
+            _associations.Add(key, toShortDateString);
+            _timeOfInsertions.Add(key, System.DateTime.Now);
+        }
+
+        public int Count
+        {
+            get { return _associations.Count; }
+        }
+
+        public int SearchStats(string key)
+        {
+            if (_searchStats.ContainsKey(key))
+                return _searchStats[key];
+            return 0;
+        }
+
+        public string Find(string key)
+        {
+
+            AddToSearchStats(key);
+
+            if (_associations.ContainsKey(key))
+                return _associations[key];
+
+            return null;
+        }
+
+        private void AddToSearchStats(string key)
+        {
+            if (!_associations.ContainsKey(key))
+                return;
+
+            if (!_searchStats.ContainsKey(key))
+            {
+                _searchStats.Add(key, 0);
+            }
+
+            _searchStats[key] = _searchStats[key] + 1;
+        }
+    }
+}
